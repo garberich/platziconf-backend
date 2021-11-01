42f7a2851a9a0917d599e73b8a85f15842bbf2f2
Richard <rgarcia@ubits.co>
2021-10-31 22:07:27 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '155820e0ff5b129daa6618de872584a2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f4f199a5-87fb-4b9a-bd8c-53e4ed0a40b2'
+};
